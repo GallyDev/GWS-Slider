18bb611adb0789ff32c5869c0468e122c1a30086
René Jossen <rene@enjor.ch>
2025-11-20 10:47:07 +0100

    V1.0.2 - Aktualisiere Versionsnummer auf 1.0.2 und stelle sicher, dass flex-wrap auf nowrap gesetzt ist

--- functions.php
+++ functions.php
@@ -1,6 +1,6 @@
 <?php
 	// this file will be included by GWS-Debugian automatically
-	define('GWS_SLIDER_VERSION', '1.0.1');
+	define('GWS_SLIDER_VERSION', '1.0.2');
 	// MESSAGE_INFO for the Git-Commit-Message: Erster Klick im Pagination-Thumbnail war kaputt
 	// Use this format to generate Git-Commit-Message: "Vx.x.x - MESSAGE_INFO"
 	// The Git-Messages must be in german
